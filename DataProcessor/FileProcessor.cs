c3417de823acb2684314ae613183abc138f96af9
msg4edson <msg.edson@gmail.com>
2022-02-07 19:16:09 -0300

    Fixes #1

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor
+{
+    class FileProcessor
+    {
+        public string InputFilePath { get; }
+
+        public FileProcessor(string filePath) => InputFilePath = filePath;
+       
+        public void Processor()
+        {
+            Console.WriteLine($"Begin process of { InputFilePath}");
+
+            // Check if file exists
+            //if (!File.Exists())
+            //{
+
+            //}
+        }
+    }
+}
---
+++ DataProcessor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parsing command line options");
+
+            // Command line validation omitted for brevity
+
+            var command = args[0];
+
+            if (command == "--file")
+            {
+                var filePath = args[1];
+                Console.WriteLine($"Single file {filePath} selected");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                Console.WriteLine($" Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command line oprions");
+            }
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadLine();
+        }
+
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Processor();
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            
+        }
+    }
+}
